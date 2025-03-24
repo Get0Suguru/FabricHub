949254f2c5ba95d6e1b2ec4e8eab0cb215fc9a06
Gautam Taneja <gautam45taneja@gmail.com>
2025-03-24 21:10:44 +0530

    create order management models with payment integration

---
+++ fabricHub/src/main/java/com/geto/fabricHub/model/PaymentDetails.java
@@ -0,0 +1,21 @@
+package com.geto.fabricHub.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentDetails {
+
+    private String paymentMethod;
+    private String status;
+    private String paymentId;
+    private String razorpayPaymentLinkId;
+    private String razorpayLinkReferenceId;
+    private String razorpayLinkStatus;
+    private String razorpayId;
+}
